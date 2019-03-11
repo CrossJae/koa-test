390fb9af7cbdf000276e25dd6470504166656beb
crossjas_mac <crossjae@163.com>
2019-03-11 23:36:26 +0800

    feat: 再增加一级app.use

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const app = new Koa()
 app.use(async (ctx, next) => {
     console.log('step 1')
     await next()
-    console.log('step 3')
+    console.log('step 5')
+})
+
+app.use(async (ctx, next) => {
+    const start = Date.now()
+    console.log('step 2')
+    await next()
+    const ms = Date.now() - start
+    console.log('step 4 ，共需要' + ms + '毫秒')
 })
 
 app.use(async ctx => {
-    console.log('step 2')
+    console.log('step 3')
     ctx.body = 'Hello World!'
 })
 
