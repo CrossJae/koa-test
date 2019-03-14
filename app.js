3ddbc94df4f79b6d4c173050ad39d65dd4b486b6
crossjas_mac <crossjae@163.com>
2019-03-14 09:32:56 +0800

    feat: ctx.app & ctx.app.emit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
 
+// 监听一个事件
+app.on('getUrl', (value) => {
+    console.log(value)
+})
+
 app.use(async (ctx) => {
-    console.log(ctx.req) //在本行断点，可以查看req或者res的方法
+    console.log('ctx.app:::>', ctx.app) // { subdomainOffset: 2, proxy: false, env: 'development' }
+    // 发出一个事件
+    // 一般用途：手动释放error事件
+    ctx.app.emit('getUrl', ctx.req.url)
 })
 
 app.listen(3000)
