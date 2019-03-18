f2f4b2f604aeb2db6b9626479f82c0cc4ed06c6a
crossjas_mac <crossjae@163.com>
2019-03-18 23:05:14 +0800

    feat: add nodemon用来监听app.js / add error throw

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
 
-// 监听一个事件
-app.on('getUrl', (value) => {
-    console.log(value)
+app.use(async (ctx, next) => {
+    // ctx.throw('name required!') // 默认是500，server会报错
+    // ctx.throw(400, 'name required!!!')
+    ctx.state.user = 'crossjae'
+    await next()
 })
 
-app.use(async (ctx) => {
-    console.log('ctx.app:::>', ctx.app) // { subdomainOffset: 2, proxy: false, env: 'development' }
-    // 发出一个事件
-    // 一般用途：手动释放error事件
-    ctx.app.emit('getUrl', ctx.req.url)
+app.use( ctx => {
+    ctx.assert(ctx.state.user, 401, '未登录！')
+    ctx.body = `hi, ${ctx.state.user}!`
 })
 
 app.listen(3000)
